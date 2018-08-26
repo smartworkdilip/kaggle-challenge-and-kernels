358543347275e2a0780c92ab1b313b242e4c3014
Dilip Kuamr <41826086+smartworkdilip@users.noreply.github.com>
2018-08-26 16:53:18 +0530

    Add files via upload

---
+++ cat_classifier.py
@@ -0,0 +1,141 @@
+mport time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+from dnn_app_utils_v2 import *
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) 
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+%load_ext autoreload
+%autoreload 2
+
+np.random.seed(1)
+
+
+train_x_origtrain_x , train_y, test_x_orig, test_y, classes = load_data()
+
+
+
+index = 10
+plt.imshow(train_x_orig[index])
+print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+
+
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(train_x_orig.shape))
+print ("train_y shape: " + str(train_y.shape))
+print ("test_x_orig shape: " + str(test_x_orig.shape))
+print ("test_y shape: " + str(test_y.shape))
+
+
+ 
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
+n_x = 12288     
+n_h = 7
+n_y = 1
+layers_dims = (n_x, n_h, n_y)
+
+def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
+    """
+    Implements a two-layer neural network: LINEAR->RELU->LINEAR->SIGMOID.
+    
+    Arguments:
+    X -- input data, of shape (n_x, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- dimensions of the layers (n_x, n_h, n_y)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- If set to True, this will print the cost every 100 iterations 
+    
+    Returns:
+    parameters -- a dictionary containing W1, W2, b1, and b2
+    """
+    
+    np.random.seed(1)
+    grads = {}
+    costs = []                              
+    m = X.shape[1]                       
+    (n_x, n_h, n_y) = layers_dims
+    
+    
+    parameters = initialize_parameters(n_x,n_h,n_y)
+ 
+    
+    
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+   
+
+    for i in range(0, num_iterations):
+
+       
+        A1, cache1 = linear_activation_forward(X,W1,b1,"relu")
+        A2, cache2 = linear_activation_forward(A1,W2,b2,"sigmoid")
+      
+        
+        
+        cost = compute_cost(A2,Y)
+        
+        
+     
+        dA2 = - (np.divide(Y, A2) - np.divide(1 - Y, 1 - A2))
+
+
+        dA1, dW2, db2 = linear_activation_backward(dA2,cache2,"sigmoid")
+        dA0, dW1, db1 = linear_activation_backward(dA1,cache1,"relu")
+        
+        
+        
+        grads['dW1'] = dW1
+        grads['db1'] = db1
+        grads['dW2'] = dW2
+        grads['db2'] = db2
+        
+     
+        parameters = update_parameters(parameters,grads,learning_rate)
+       
+
+        
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        
+      
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+       
+  
+
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
